86cfbcaeabede4a97446afae6c0f647ef5013a45
EC2 Default User <ec2-user@ip-172-31-13-161.ap-northeast-1.compute.internal>
2023-04-19 13:11:02 +0000

    管理者のジャンルページと商品ページ一部

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,27 +1,34 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-
   end
 
-  def show
-
-  end
-
-  def edit
-
+  def new
+    @item = Menu.new
   end
 
   def create
+    @item = Menu.new(menu_params)
+    @item.save
+    redirect_to admin_items_path
+  end
 
+  def show
+  end
+
+  def edit
   end
 
   def update
-
   end
 
   def destroy
+  end
 
+  private
+
+  def menu_params
+    params.require(:menu).permit( :image, :name, :description, :genre_id, :price, :sale_status)
   end
 
 end
