887356d8aa902bc5f838fecb575b9a15a77d1d80
tashirohiroki45483 <kirigiri700@gmail.com>
2023-04-20 16:49:19 +0000

    pblicの商品ページのジャンル検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,9 +2,16 @@ class Public::ItemsController < ApplicationController
 
   def index
     @menus = Menu.all
+    @genres = Genre.all
+    if params[:genre_id]
+      @menus = Menu.where(genre_id: params[:genre_id])
+    else
+      @menus = Menu.all
+    end
   end
 
   def show
     @menu = Menu.find(params[:id])
+    @genres = Genre.all
   end
 end
