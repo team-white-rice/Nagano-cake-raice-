b085da4a66c39628aa74dfeea67fcdcd8873e792
nakazawa <zhongzejiangji@gmail.com>
2023-04-20 08:19:22 +0000

    cart_items add

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,22 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
+  def create
+  end
+
+  private
+
+  def cart_item_params
+  end
 end
