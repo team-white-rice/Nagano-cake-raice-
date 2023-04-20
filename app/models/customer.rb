8a22109ce068e8ff5c78618cb989c48e450d3dad
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-04-20 12:32:44 +0000

    show edit unsubcribe

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,14 @@ class Customer < ApplicationRecord
   has_many :deliveries, dependent: :destroy
   has_many :orders, dependent: :destroy
 
-  def active_for_authentication?
-   super && (status == false)
-  end
+
+  validates :last_name, length: { minimum: 2 }
+  validates :first_name, length: { minimum: 2 }
+  validates :last_name_kana, length: { minimum: 2 }
+  validates :first_name_kana, length: { minimum: 2 }
+  validates :postal_code, length: { minimum: 2 }
+  validates :address, length: { minimum: 2 }
+  validates :phone_number, length: { minimum: 2 }
+  validates :email, length: { minimum: 2 }, uniqueness: true
+
 end
