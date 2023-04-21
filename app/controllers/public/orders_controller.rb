cad00e171f6e37c1796cc90ab553317fb6a7a17b
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-04-21 08:03:30 +0000

    order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,4 +6,31 @@ class Public::OrdersController < ApplicationController
     @delivery = Delivery.all
   end
 
+  def confirm
+    @order = Oder.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @delivery = Delivery.find(params[:order][:address_id])
+      @order.postal_code =  @delivery.postal_code
+      @order.address =  @delivery.address
+      @order.name =  @delivery.name
+    elsif params[:order][:select_address] == "2"
+      @order.customer = current_customer
+    end
+      @cart_items = current_customer.cart_items
+      @order_new = Order.new
+      render :confirm
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_methods, :postal_code, :address, :name)
+  end
+
+
 end
