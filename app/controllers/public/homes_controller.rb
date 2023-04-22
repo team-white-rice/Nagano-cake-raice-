28d0f9e725e436af22145f910fe62e050e6648fd
tashirohiroki45483 <kirigiri700@gmail.com>
2023-04-22 11:07:57 +0000

    ヘッダーとフッター

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @menu = Menu.all.order(id: "DESC").limit(4) ## idの降順
+    @menus = Menu.all.order(id: "DESC").limit(4)
     @genres = Genre.all
   end
 end
