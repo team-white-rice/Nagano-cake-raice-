6c3c8e2e1dc73c5c6a1f79698222e34b40b45114
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-04-22 08:19:55 +0000

    order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,19 +6,27 @@ class Public::OrdersController < ApplicationController
     @delivery = Delivery.all
   end
 
+  def show
+  end
+
+  def index
+  end
+
+
   def confirm
-    @order = Oder.new(order_params)
-    if params[:order][:select_address] == "0"
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_address] == "1"
+    @order = Order.new(order_params)
+    #@delivery = Delivery.find(params[:order][:address_id])
+    if params[:order][:select_delivery] == "0"
+      @order.postcode = current_customer.postal_code
+      @order.ship_to_address = current_customer.address
+      @order.ship_name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_delivery] == "1"
       @delivery = Delivery.find(params[:order][:address_id])
-      @order.postal_code =  @delivery.postal_code
-      @order.address =  @delivery.address
-      @order.name =  @delivery.name
-    elsif params[:order][:select_address] == "2"
-      @order.customer = current_customer
+      @order.postcode =  @delivery.post_address
+      @order.ship_to_address =  @delivery.address
+      @order.ship_name =  @delivery.name
+    elsif params[:order][:select_delivery] == "2"
+      @order.customer_id = current_customer.id
     end
       @cart_items = current_customer.cart_items
       @order_new = Order.new
@@ -26,10 +34,41 @@ class Public::OrdersController < ApplicationController
   end
 
 
+  def create
+    @order = Order.new(order_params)
+    @order.save!
+    @cart_items = current_customer.cart_items.all
+
+    @cart_items.each do |cart_item|
+      @order_details = OrderDetail.new
+      @order_details.order_id = @order.id
+      @order_details.menu_id = cart_item.menu.id
+      @order_details.item_price = cart_item.item_quantity.price_excluding_tax
+      @order_details.quantity = cart_item.item_quantity
+      @order_details.production_status = 0
+      @order_details.save!
+    end
+
+    CartItem.destroy_all
+    redirect_to complete_orders_path
+
+  end
+
+
+
+
   private
 
   def order_params
-    params.require(:order).permit(:payment_methods, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postcode, :ship_to_address, :ship_name, :customer_id , :status, :freight, :total_price)
+  end
+
+
+  def cartitem_nill
+    cart_items = current_customer.cart_items
+    if cart_items.blank?
+      redirect_to complete_orders_path
+    end
   end
 
 
